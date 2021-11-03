3ae559fc458373ffcac2c505b912a79880026a75
idenk <idenkim@gmail.com>
2021-11-04 00:18:46 +0900

    study

--- programmers/lv2/오픈채팅방.js
+++ programmers/lv2/오픈채팅방.js
@@ -8,8 +8,10 @@ function solution(record) {
       stateList.push([id, '님이 들어왔습니다.']);
     } else if (state === 'Leave') {
       stateList.push([id, '님이 나갔습니다.']);
-    } 
-    nameList[id] = name;
+    }
+    if (name) {
+      nameList[id] = name;
+    }
   });
 
   const answer = stateList.map((el) => `${nameList[el[0]]}${el[1]}`);
@@ -17,6 +19,12 @@ function solution(record) {
   return answer;
 }
 
-const record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"];
+const record = [
+  'Enter uid1234 Muzi',
+  'Enter uid4567 Prodo',
+  'Leave uid1234',
+  'Enter uid1234 Prodo',
+  'Change uid4567 Ryan',
+];
 const result = solution(record);
 console.log(result);
